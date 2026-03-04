a53a3f4f3c963807172ae6f1b09735c3250b6017
BrayanthMatul <brayanth.matul146@gmail.com>
2026-03-04 08:21:45 -0600

    Usuario BD

--- Primera_Practica_Codigo/database/script.sql
+++ Primera_Practica_Codigo/database/script.sql
@@ -1,9 +1,13 @@
 -- Base de datos para Pizza Express Tycoon
 CREATE DATABASE pizza_express_tycoon;
 
+CREATE USER 'dbpractica1'@'localhost' IDENTIFIED BY '12345';
+GRANT ALL PRIVILEGES ON pizza_express_tycoon.* TO 'dbpractica1'@'localhost';
+FLUSH PRIVILEGES;
+
 USE pizza_express_tycoon;
 
-CREATE TABLE Sucursal (
+CREATE TABLE sucursal (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(255) NOT NULL,
     ubicacion VARCHAR(255) NOT NULL,
